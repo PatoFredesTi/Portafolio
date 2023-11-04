f09f74b46b179c0e7cbd1fd0dbb8975f6b279391
Patricio Fredes <pfredes@anguitaosorio.cl>
2023-11-04 03:45:44 -0300

    Se arreglan un detalle de skills donde ahora se muestra maximo 5 elementos por linea

--- src/components/Styled/Styled.Skills.tsx
+++ src/components/Styled/Styled.Skills.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const SkillsSection = styled.div`
@@ -19,15 +18,19 @@ export const SkillsContainers = styled.div`
     flex-wrap: wrap;
     gap: 20px;
     margin-top: 30px;
+    max-width: 1200px; 
 `;
 
 export const SkillCard = styled.div`
     background-color: ${({ theme }) => theme.backgroundColorCard};
     padding: 20px;
     border-radius: 10px;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     transition: transform 0.3s ease;  
     cursor: pointer;
+    width: calc(20% - 20px); 
+    margin-bottom: 20px; 
+
     &:hover{
         transform: scale(1.1);
     }
@@ -36,9 +39,11 @@ export const SkillCard = styled.div`
 export const SkillIcon = styled.span`
     font-size: 24px;
     margin-bottom: 10px;
+    display: block;
 `;
 
 export const SkillName = styled.p`
     font-size: 20px;
     font-weight: 600;
 `;
+
